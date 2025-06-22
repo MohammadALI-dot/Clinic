aca3597a5e3e7faf5ba8254a1a69b193758a106a
Mohammad Ali Yassine <mhmdaliyassine3@gmail.com>
2025-06-22 23:31:34 +0300

    Update database.js

--- utils/database.js
+++ utils/database.js
@@ -12,16 +12,24 @@
 
 
 //for web
-require("dotenv").config(); // Load environment variables from .env
+// require("dotenv").config(); // Load environment variables from .env
 
-const mysql = require("mysql2");
+// const mysql = require("mysql2");
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+// const pool = mysql.createPool({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASS,
+//     database: process.env.DB_NAME,
+//     port: process.env.DB_PORT
+// });
+
+const mysql = require('mysql2');
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || '1234',
+  database: process.env.DB_NAME || "about_patients"
 });
 
 module.exports = pool.promise();
